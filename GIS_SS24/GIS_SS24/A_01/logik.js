f2dccafeb055e90b58665f14cea17fe9d9df845a
Barc1337 <164857091+Barc1337@users.noreply.github.com>
2024-04-21 16:37:36 +0200

    Add files via upload

---
+++ GIS_SS24/GIS_SS24/A_01/logik.js
@@ -0,0 +1,7 @@
+<script src="https://www.paypal.com/sdk/js?client-id=BAA6nFeLIK5c2XpIyb9Ahq7Wd8xeVgDFTFFX1XwV10XSnDJLiJrrM7g7hZbcQjFPJnI_yGPG_oZ0mv-qys&components=hosted-buttons&disable-funding=venmo&currency=EUR"></script>
+<div id="paypal-container-LESNPCFHB7MDY"></div>
+<script>
+  paypal.HostedButtons({
+    hostedButtonId: "LESNPCFHB7MDY",
+  }).render("#paypal-container-LESNPCFHB7MDY")
+</script>
