e1b94c8755c3dfe9fb1e25a4c63955141f5dad50
Barc1337 <164857091+Barc1337@users.noreply.github.com>
2024-05-28 13:08:00 +0200

    Add files via upload

---
+++ A_01/java.js
@@ -0,0 +1,32 @@
+    // Fügt bezeichnungen für jedes Smartphone ein inkl. Bild 
+window.onload = function() {
+    var smartphones = [
+        {name: "Apple Iphone 15 128 GB", price: 799, image: "https://cdn.idealo.com/folder/Product/203247/5/203247542/s1_produktbild_mittelgross/apple-iphone-15-128gb-schwarz.jpg" },
+        {name: "Apple Iphone 15 Pro 128 GB", price: 899, image: "https://cdn.idealo.com/folder/Product/203248/7/203248713/s1_produktbild_mittelgross/apple-iphone-15-pro-128gb-titan-schwarz.jpg" },
+        {name: "Apple Iphone 15 Pro Max 128 GB", price: 1199, image: "https://cdn.idealo.com/folder/Product/203248/6/203248693/s1_produktbild_mittelgross/apple-iphone-15-pro-max-256gb-titan-schwarz.jpg" },
+    ];
+
+    var smartphonesDiv = document.getElementById("smartphones");
+
+    // Fügt jedes Smartphone-Array in ein eigenes Div ein und erstellt ein übergeordnetes Div
+    smartphones.forEach(function(smartphone) {
+        var smartphoneElement = document.createElement("div");
+        smartphoneElement.innerHTML = "<h3>" + smartphone.name + "</h3><img src='" + smartphone.image + "' alt='" + smartphone.name + "'><p>Preis: " + smartphone.price + " €</p><button onclick='addToCart(\"" + smartphone.name + "\", " + smartphone.price + ")'>In den Warenkorb legen</button>";
+        smartphonesDiv.appendChild(smartphoneElement);
+    });
+};
+
+    // Funktion zum Hinzufügen des ausgewählten Smartphones zum Warenkorb
+function addToCart(name, price) {
+    // Holt den aktuellen Warenkorb oder erstellt einen neuen, wenn keiner vorhanden ist
+    var cart = JSON.parse(localStorage.getItem("cart")) || [];
+    
+    alert(name + " wurde zum Warenkorb hinzugefügt!");
+
+    // Fügt das ausgewählte Smartphone zum Warenkorb hinzu
+    var item = { name: name, price: price };
+    cart.push(item);
+
+    // Speichert den aktualisierten Warenkorb im localStorage
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
---
+++ A_01/java1.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    displayCart();
+
+    // Verknüpfung der Bezahl-Buttons mit der handlePayment-Funktion
+    document.getElementById('paypal-button').addEventListener('click', handlePayment);
+    document.getElementById('sofort-button').addEventListener('click', handlePayment);
+    document.getElementById('creditcard-button').addEventListener('click', handlePayment);
+};
+
+// Funktion zum Anzeigen des Warenkorbs
+function displayCart() {
+    var cartItemsElement = document.getElementById("cart-items");
+    cartItemsElement.innerHTML = ""; // Leere den aktuellen Warenkorb-Inhalt
+
+    // Holt den aktuellen Warenkorb aus dem localStorage
+    var cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    // Fügt jeden Artikel im Warenkorb der Anzeige hinzu
+    cart.forEach(function(item) {
+        var li = document.createElement("li");
+        li.textContent = item.name + " - Preis: " + item.price + " €";
+        cartItemsElement.appendChild(li);
+    });
+}
+// Funktion zum Leeren des Warenkorbs
+function clearCart() {
+    localStorage.removeItem("cart");
+}
+
+// Funktion zum Handhaben der Zahlung
+function handlePayment(event) {
+    // Warenkorb leeren
+    clearCart();
+
+    // Weiterleitung zur Zahlungsbestätigung
+    window.location.href = 'Zahlungsbestätigung.html';
+}
